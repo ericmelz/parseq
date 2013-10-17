6ce3b8a95d950eb28c95348d63947b7315cc6037
Chris Pettitt <cpettitt@linkedin.com>
2013-10-17 15:36:33 -0700

    Pretty significant rewrite to tracevis
    Highlights:
    
    * We now have a test coverage report for the library code in tracevis
    * Improved test coverage
    * Simplified many of the transforms by keeping everything in graph form
      and applying transforms (e.g. for filtering)
    * Removed dig.js - it's been EOL'd
    * Added graphlib.js - replaces dig.js
    * Added graphlib-dot.js - used for rendering graphviz output
    * Renamed trace.html to index.html
    * Tracevis now builds its package itself

---
+++ tools/tracevis/lib/trace/d3Treeify.js
@@ -0,0 +1,58 @@
+module.exports = d3Treeify;
+
+/*
+ * Creates a representation of the graph that can be used for hierarchical
+ * layouts with d3. There should be no direct dependency on D3 in this module.
+ */
+function d3Treeify(g) {
+  var minStart = Math.min.apply(Math,
+                                g.nodes().map(function(u) {
+                                  return g.node(u).startNanos;
+                                }));
+
+  if (g.children(null).length === 0) {
+    throw new Error('Input graph must have at least one node!');
+  }
+
+  function dfs(u) {
+    var uValue = g.node(u),
+        children = [];
+
+    g.children(u).forEach(function(v) {
+      children.push(dfs(v));
+    });
+
+    // Sort children by start time. This is used implicitly by
+    // d3.layout.hierarchy layer.
+    sortByOrder(children);
+
+    var result = {
+      id: u,
+      name: uValue.name,
+      start: Math.round((uValue.startNanos - minStart) / (1000 * 1000)),
+      elapsed: Math.round((uValue.endNanos - uValue.startNanos) / (1000 * 1000)),
+      value: uValue.value,
+      resultType: uValue.resultType,
+      order: uValue.order
+    };
+    if (children.length) {
+      result.children = children;
+    }
+    return result;
+  }
+
+  return {
+    id: null,
+    name: '',
+    start: 0,
+    elapsed: 0,
+    value: '',
+    resultType: '',
+    order: -1,
+    children: sortByOrder(g.children(null).map(dfs))
+  };
+}
+
+function sortByOrder(tasks) {
+  return tasks.sort(function(a, b) { return a.order - b.order; });
+}
